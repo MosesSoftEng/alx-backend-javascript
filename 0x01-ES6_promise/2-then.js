b7cde922eb666d3fdb96cdaf52ab189842f28bda
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-12-22 12:47:29 +0300

    Add solution to task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,10 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({ status: 200, body: 'success' }))
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
     .catch(() => new Error())
+    // eslint-disable-next-line no-console
     .finally(() => console.log('Got a response from the API'));
 }
