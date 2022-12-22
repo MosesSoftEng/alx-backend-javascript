a3d40b9fe36b656044cb99d1a896d75b3a6a395a
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-12-22 13:01:38 +0300

    Add solution to task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-console */
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise
-    .all([uploadPhoto(), createUser()])
-    .then((res) => {
+    .all([
+      uploadPhoto(),
+      createUser(),
+    ]).then((res) => {
       console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
-    })
-    .catch(() => console.log('Signup system offline'));
+    }).catch(() => console.log('Signup system offline'));
 }
