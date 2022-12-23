0d93075391dac5e7691f5dca163bfe55bc98d17c
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-12-23 08:18:30 +0300

    Update solution to task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import Currency from './3-currency';
 
 export default class Pricing {
@@ -10,41 +11,38 @@ export default class Pricing {
     return this._amount;
   }
 
-  set amount(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('amount must be a number');
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
     }
-    this._amount = value;
+
+    this._amount = newAmount;
   }
 
-  /**
-   * @returns {Currency}
-   */
   get currency() {
     return this._currency;
   }
 
-  /**
-   * @param {Currency} value
-   */
-  set currency(value) {
-    if (!(value instanceof Currency)) {
-      throw new TypeError('currency must be a Currency');
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError('Currency must be of type Currency');
     }
-    this._currency = value;
+
+    this._currency = newCurrency;
   }
 
   displayFullPrice() {
-    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
   }
 
   static convertPrice(amount, conversionRate) {
     if (typeof amount !== 'number') {
-      throw new TypeError('amount must be a number');
+      throw new TypeError('Amount must be a number');
     }
     if (typeof conversionRate !== 'number') {
-      throw new TypeError('conversionRate must be a number');
+      throw new TypeError('ConversionRate must be a number');
     }
+
     return amount * conversionRate;
   }
 }
