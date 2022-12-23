bdfd5476e727fb49fccf68f0989b8536904e7c47
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-12-23 07:54:19 +0300

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-underscore-dangle */
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building
-      && typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error(
-        'Class extending Building must override evacuationWarningMessage',
-      );
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
 
     this.sqft = sqft;
