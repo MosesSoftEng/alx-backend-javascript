4bf981f1b572f6e861812a5f643037ebaff5e995
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2023-01-03 23:22:01 +0300

    Update module docs

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,9 +1,9 @@
 /**
- * A function to check if all the elements in the array exist within the set.
- * @param {*} set set.
- * @param {*} array array.
- * @returns true if all the elements in the array exist within the set.
- *  otherwise false
+ * Check if a set has all of the elements from an array.
+ * @param {Set} set - The set to check.
+ * @param {Array} array - The array of elements to check.
+ * @return {boolean} - `true` if all elements are contained in the set,
+ *                     `false` otherwise.
  */
 export default function hasValuesFromArray(set, array) {
   return array.every((element) => set.has(element));
