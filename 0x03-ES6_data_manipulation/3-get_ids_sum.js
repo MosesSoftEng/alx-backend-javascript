e7b904b2cdb16c87ba4828b044cc3c9958698796
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2023-01-03 23:22:33 +0300

    Refactor code

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,12 +1,14 @@
 /**
  * A function that returns the sum of all the student ids.
  * @param {Array} students list of student objects.
- * @returnssum sum of all the student ids otherwise, empty array.
+ * @param {String} city name of city.
+ * @returns array of objects who are located in a specific city otherwise,
+ *  empty array.
  */
-export default function getListStudents(students) {
+export default function updateStudentGradeByCity(students, city, newGrades) {
   if (students instanceof Array) {
-    return students.reduce((total, student) => total + student.id, 0);
+    return students.filter((student) => student.location === city);
   }
 
-  return 0;
+  return [];
 }
