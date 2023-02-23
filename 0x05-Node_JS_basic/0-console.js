7528d9a934f0db79c2054f79d98812461d88620c
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2023-02-23 06:42:19 +0300

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,8 +1,3 @@
-/**
- * Displays a message on the STDOUT.
- * @param {String} msg The message to display.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- */
 const displayMessage = (msg) => {
   console.log(msg);
 };
