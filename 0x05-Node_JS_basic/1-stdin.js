2a97fc5b84f35c32e917542260536a1ee0ec10aa
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2023-02-25 15:11:57 +0300

    Remove new line at the end

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
-console.log('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
