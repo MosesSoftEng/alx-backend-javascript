2a6446118aac29c5b4cf7a45bfc7aa373d98582d
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2023-02-25 15:09:21 +0300

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data}`);
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
